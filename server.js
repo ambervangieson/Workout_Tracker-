b0eb7b4d4c38d15e4c5ad49bc9a39c31397f8c51
Amber VanGieson <ambervangieson@gmail.com>
2021-05-10 18:24:52 -0400

    updated server

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
 
-const { Workout } = require("./models");
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
